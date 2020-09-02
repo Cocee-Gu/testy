d05448b0645e76d2d73cf10dab2e6e2321babec9
Cocee-Gu <303013205@qq.com>
2020-09-02 15:06:36 +0800

    对接2

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,196 +1,319 @@
-//index.js
-//获取应用实例
-const app = getApp(); 
-import { request, uploadFile } from "../../request/request.js";
-
+// pages/SHB/index/index.js
+var template = require("../tabbar_template/index.js");
+import {
+	request
+} from '../../../request/request.js'
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    // 轮播图
-    carouselArr: [
-      "https://i2.hdslb.com/bfs/archive/0e4e8e334df8a75dd45b37e5b5c34d4e8ed393ce.jpg",
-      "https://i1.hdslb.com/bfs/archive/a1e489be5d9cdb28a453b02c305177102cd29c72.jpg",
-      "https://i0.hdslb.com/bfs/archive/0febe569d0a2f11abdcd6c2187783ddc76d56e00.jpg",
-      "https://i2.hdslb.com/bfs/archive/9e559c6d0bd9c81e9e126be123f20b9e3283438f.jpg",
-    ],
-    // 轮播图当前中间的图片的索引
-    carouselIndex: 0,
-    // 工具栏数据
-    toolsArr: [
-      { image: "", name: "二手书城", url: "../secondHandBook/index/index" },
-      { image: "", name: "失物招领", url: "'../lostandfound/lostIndex/index'" },
-      { image: "", name: "报名助手", url: "" },
-      { image: "", name: "待定", url: "" },
-      { image: "", name: "码上报名", url: "" },
-    ],
-    // 任务栏数据
-    missionArr: {
-      image: "../../image/tool-kaixueHelper.png",
-      name: "报名助手",
-      mission: ["缴纳学费", "学院签到"],
-    },
-    //判断课程表日程表是否为空
-    isCurriculum: false,
-    isSchedule: false,
-    //是否显示  课程表   日程
-    showCurriculum: true,
-    showSchedule: true,
+	/**
+	 * 页面的初始数据
+	 */
+	bookGo(e){
+		console.log(e)
+	},
+	getServerData() {
+		const res = request({
+		  url: "book-market/userBook/"+(this.data.isBuy?'sell':'borrow')+'?pageIndex=1&pageSize=100',
+		  data: {},
+		  method: "get"
+		})
+		res.then((res1)=>{
+			let arr=[]
+			for (let s of res1.data.data) {
+				
+					s.img= s.imageUrls
+					s.name= s.bookName
+					s.author= s.title
+					s.inf= s.address
+					s.price= s.sellPrice
+					s.ori_price= 40
+					s.new= s.degree+"成新"
+				
+			}
+			this.setData({
+				booksArr:res1.data.data
+			})
+			
+			
+			
+			
+		})
+	},
+	
+	data: {
+		// 首页轮播图
+		carouselIndex: 0,
+		swiper_index: [{
+				img: "/image/secondHandBook_image/swiper01.png"
+			},
+			{
+				img: "/image/secondHandBook_image/swiper02.png"
+			},
+			{
+				img: "/image/secondHandBook_image/swiper03.png"
+			},
+		],
+		// 判断当前是买书还是借阅的页面 true 买书 false 借阅
+		isBuy: true,
+		// 买书页面数据
+		booksArr: [{
+				img: "/image/secondHandBook_image/book01.png",
+				name: "百年孤独",
+				author: "加西亚马尔克斯",
+				inf: "仅翻阅，无笔记，无损坏",
+				price: 10,
+				ori_price: 40,
+				new: "九成新",
+			},
+			{
+				img: "/image/secondHandBook_image/book02.png",
+				name: "百年孤独",
+				author: "加西亚马尔克斯",
+				inf: "仅翻阅，无笔记，无损坏",
+				price: 10,
+				ori_price: 40,
+				new: "九成新",
+			},
+			{
+				img: "/image/secondHandBook_image/book03.png",
+				name: "百年孤独",
+				author: "加西亚马尔克斯",
+				inf: "仅翻阅，无笔记，无损坏",
+				price: 10,
+				ori_price: 40,
+				new: "九成新",
+			},
+			{
+				img: "/image/secondHandBook_image/book01.png",
+				name: "百年孤独",
+				author: "加西亚马尔克斯",
+				inf: "仅翻阅，无笔记，无损坏",
+				price: 10,
+				ori_price: 40,
+				new: "九成新",
+			},
+			{
+				img: "/image/secondHandBook_image/book02.png",
+				name: "百年孤独",
+				author: "加西亚马尔克斯",
+				inf: "仅翻阅，无笔记，无损坏",
+				price: 10,
+				ori_price: 40,
+				new: "九成新",
+			},
+			{
+				img: "/image/secondHandBook_image/book03.png",
+				name: "百年孤独",
+				author: "加西亚马尔克斯",
+				inf: "仅翻阅，无笔记，无损坏",
+				price: 10,
+				ori_price: 40,
+				new: "九成新",
+			},
+		],
+		// 底部输入部分
+		isShow_hand: false,
+		isShow_isn_error: false,
+		isShow_by_isn: false,
+		//是否显示蒙层
+		isShow_back: false,
 
-    //  课程信息
-    curriculumArr: {
-      time: "星期三/第一周",
-      classInf: [
-        { part1: "1", part2: "2", name: "网站设计与开发", add: "c-374" },
-        { part1: "3", part2: "4", name: "多媒体程序设计", add: "c-366" },
-        { part1: "5", part2: "6", name: "多媒体程序设计", add: "c-366" },
-        { part1: "7", part2: "8", name: "多媒体程序设计", add: "c-366" },
-      ],
-    },
+		iSN: null, // ISN 码
+	},
+	/**
+	 * 更新输入的ISN码
+	 */
+	updateISN(e) {
+		const value = e.detail.value
+		this.setData({
+			iSN: value
+		})
+	},
 
-    // 日程信息
-    scheduleArr: [
-      {
-        date: "2020/07/29",
-        week: "周四",
-        inf: "聚会聚会聚会聚会聚会聚会聚会聚会聚会聚会",
-        add: "钟楼",
-        time: "20:00",
-        when: "30日",
-      },
-      {
-        date: "2020/07/29",
-        week: "周四",
-        inf: "聚会",
-        add: "钟楼",
-        time: "20:00",
-        when: "30日",
-      },
-      {
-        date: "2020/07/30",
-        week: "周五",
-        inf: "写代码",
-        add: "在家里",
-        time: "7:00",
-        when: "30日",
-      },
-    ],
-  },
+	/**
+	 * 通过ISN码查找书籍信息
+	 */
+	searchISN() {
+		if (this.data.iSN.length != 13) {
+			return wx.showToast({
+				title: '请重新确认ISN码！',
+				icon: 'none'
+			})
+		}
+		wx.request({
+			url: 'https://book.feelyou.top/isbn/' + this.data.iSN,
+			method: 'GET',
+			dataType: 'json',
+			responseType: 'text',
+			success: function(res) {
+				console.log(res)
+				if (res.data.title == undefined) {
+					console.log("请重新确认ISN码！")
+				} else {
+					const title = res.data.title == undefined ? '暂无信息' : res.data.title;
+					const auther = res.data.book_info.作者 == undefined ? '暂无信息' : res.data.book_info.作者;
+					const press = res.data.book_info.出版社 == undefined ? '暂无信息' : res.data.book_info.出版社;
+					const date = res.data.book_info.出版年 == undefined ? '暂无信息' : res.data.book_info.出版年;
+					const pricing = res.data.book_info.定价 == undefined ? '暂无信息' : res.data.book_info.定价;
+					const coverUrl = res.data.cover_url == undefined ? '暂无信息' : res.data.cover_url;
+					const bookInfo = {
+						title,
+						auther,
+						press,
+						date,
+						pricing,
+						coverUrl
+					}
+					wx.setStorageSync("bookInfo", bookInfo)
+					wx.navigateTo({
+						url: '/pages/secondHandBook/publishBook/index?flag=ISN',
+					})
+				}
 
-  /**
-   * 实现折叠功能的方法
-   * index=01：课程表
-   * index=02：日程
-   */
-  hideCurriculum: function (e) {
-    const index = e.currentTarget.dataset.choose
-    if (index === "01") {
-      this.setData({
-        showCurriculum: !this.data.showCurriculum,
-      });
-    } else if(index == "02") {
-      this.setData({
-        showSchedule: !this.data.showSchedule,
-      });
-    }
-  },
+			},
+			fail: function(res) {},
+			complete: function(res) {},
+		})
+	},
 
-  /**
-   * 点击工具栏中的内容触发
-   * index==0：二手书城
-   * index==1：失物招领
-   * index==2：报名助手
-   */
-  chooseTool: function (e) {
-    const index = e.currentTarget.dataset.index
-    switch(index) {
-      case 0:
-        wx.navigateTo({
-          url: '/pages/secondHandBook/index/index',
-        })
-      case 1:
-        wx.navigateTo({
-          url: '/pages/lostandfound/lostIndex/index',
-        })
-        break;
-    }
-  },
-
-  /**
-   * 判断课表和日程表是否为空的函数
-   * 
-   */
-  isCurriculum: function () {
-    if (this.data.curriculumArr.classInf.length === 0) {
-      this.setData({
-        isCurriculum: true,
-      });
-    }
-    if (this.data.scheduleArr.length === 0) {
-      this.setData({
-        isSchedule: true,
-      });
-    }
-  },
-
-  /**
-   * 切换轮播图时触发
-   * 返回当前轮播图中间的图片的索引
-   */
-  change: function (e) {
-    this.setData({
-      carouselIndex: e.detail.current,
-    });
-  },
+	/**
+	 * 搜索图书
+	 */
+	async searchBook(e) {
+		const value = e.detail.value
+		const requestData = {
+			book_name: value
+		}
+		const result = await request({
+			url: "book-market/searchBook",
+			method: "GET",
+			data: requestData
+		})
+		console.log(result)
+	},
 
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    /**
-     * 判断课表是否为空的函数，如果为空显示今天没有课程的内容
-     */
-    this.isCurriculum();
-  },
+	/**
+	 * 买书与借阅的切换
+	 */
+	changeChoose: function(e) {
+		console.log(e.currentTarget.dataset.index);
+		if (
+			(e.currentTarget.dataset.index === "1" && this.data.isBuy === true) ||
+			(e.currentTarget.dataset.index === "0" && this.data.isBuy === false)
+		) {
+			this.setData({
+				isBuy: !this.data.isBuy,
+			});
+			this.getServerData()
+		}
+	},
+	/**
+	 * 切换轮播图时触发
+	 * 轮播图切换，获取当前轮播图的索引
+	 */
+	change: function(e) {
+		this.setData({
+			carouselIndex: e.detail.current,
+		});
+	},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {},
+	/**
+	 * 手动输入触发
+	 */
+	input_hand: function() {
+		this.setData({
+			isShow_hand: !this.data.isShow_hand,
+			isShow_isn_error: false,
+			isShow_by_isn: false,
+			isShow_back: !this.data.isShow_hand,
+		});
+		console.log("ccc");
+		console.log(this.data.isShow_hand);
+	},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    this.getTabBar().setData({
-      tabBar: 0,
-    });
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {},
+	/**
+	 * ISN码破损触发
+	 */
+	isn_error: function() {
+		wx.navigateTo({
+			url: '/pages/secondHandBook/publishBook/index?flag=NOISN',
+		})
+		// this.setData({
+		//   isShow_hand: false,
+		//   isShow_isn_error: !this.data.isShow_isn_error,
+		//   isShow_by_isn: false,
+		// });
+	},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {},
+	/**
+	 * 输入ISN码触发
+	 */
+	by_isn: function() {
+		this.setData({
+			isShow_hand: false,
+			isShow_isn_error: false,
+			isShow_by_isn: !this.data.isShow_by_isn,
+		});
+	},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {},
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {},
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	//tabBar部分
+	onLoad: function(options) {
+		template.tabbar("tabBar", 0, this);
+	},
+
+	/**
+	 * 点击蒙层关闭
+	 */
+	quit: function() {
+		this.setData({
+			isShow_hand: false,
+			isShow_isn_error: false,
+			isShow_by_isn: false,
+			isShow_back: false,
+		});
+		console.log("quit");
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function() {},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function() {
+		this.getServerData()
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function() {},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function() {},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function() {},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function() {},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function() {},
 });
